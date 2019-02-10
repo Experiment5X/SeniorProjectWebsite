6c34f7abb4cdae0590f6c1872405d78307694949
Adam Spindler <xmeadamx@gmail.com>
2019-02-10 14:32:09 -0500

    Add app to vote on word definitions

--- Makefile
+++ Makefile
@@ -5,14 +5,18 @@ deploy: clean
 	mkdir deploy
 	mkdir deploy/css
 	mkdir deploy/images
+	mkdir deploy/script
 
 	cp style/* deploy/css/
 	cp images/* deploy/images/
+	cp script/* deploy/script/
 
 	pug templates/synopsis.pug -o deploy/
 	pug templates/index.pug -o deploy/
-	pug templates/timecards.pug -o deploy/
+	pug templates/timecards1.pug -o deploy/
+	pug templates/timecards2.pug -o deploy/
 	pug templates/domain_model.pug -o deploy/
+	pug templates/word_definition_vote.pug -o deploy/
 
 install: deploy
 	make clean
---
+++ script/word_definition_vote.js
@@ -0,0 +1,82 @@
+function initiateVoteSession(done) {
+    /*return {
+        wordId: 1,
+        word: 'like',
+        possibleDefinitions: ['SAVORINESS', 'PLEASURE', 'DESIRE', 'LOVE', 'IMITATION', 'MANNER AND MEANS']
+    };*/
+
+    $.get('http://writingstyle.ddns.net/surveyQuery', function(data) {
+        done(parseVoteResponse(data));
+    });
+}
+
+function parseVoteResponse(data) {
+    let components = data.split(';');
+    if (components[0] === 'done') {
+        return null;
+    } else {
+        var wordInfo = {
+            wordId: components[0],
+            word: components[1],
+            possibleDefinitions: components.slice(2)
+        };
+
+        return wordInfo;
+    }
+}
+
+function titleCase(word) {
+    if (word.length < 2) {
+        return word;
+    }
+    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+function handleWordSelected(event) {
+    let sender = event.target;
+    let word = $(sender).text().toUpperCase();
+        
+    vote(word, curRound);
+}
+
+function vote(word, round) {
+    let voteUrl = `http://writingstyle.ddns.net/surveyQuery?wordid=${round.wordId}&word=${round.word}&choice=${word}`;
+    $.get(voteUrl, function(data) {
+        let roundInfo = parseVoteResponse(data);
+        if (roundInfo) {
+            if (roundInfo.possibleDefinitions.length < 2) {
+                if (roundInfo.possibleDefinitions.length == 0) {
+                    vote('', roundInfo);
+                } else {
+                    vote(roundInfo.possibleDefinitions[0], roundInfo);
+                }
+            } else {
+                loadRound(roundInfo);
+            }
+        }
+    })
+}
+
+function loadRound(round) {
+    curRound = round;
+    // remove all the word buttons from the container
+    $('#word-option-container').empty();
+
+    $('#head-word').text(titleCase(round.word));
+    for (let altWord of round.possibleDefinitions) {
+        var $altWordBtn = $('<button />', {
+            'class': 'btn btn-light alt-word-button',
+            text: titleCase(altWord),
+            click: handleWordSelected,
+            type: 'button',
+        });
+
+        $('#word-option-container').append($altWordBtn);
+    }
+}
+
+$(document).ready(function() {
+    initiateVoteSession(function(round) {
+        loadRound(round);
+    });
+});
